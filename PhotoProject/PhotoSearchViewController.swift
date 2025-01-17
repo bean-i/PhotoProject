ae275ce453c0ae830a746e38984eb55e40eab56a
been <leebin0603@naver.com>
2025-01-17 17:44:23 +0900

    Feat: 탭바컨트롤러 세팅

---
+++ PhotoProject/PhotoSearchViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoSearchViewController.swift
+//  PhotoProject
+//
+//  Created by 이빈 on 1/17/25.
+//
+
+import UIKit
+
+class PhotoSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "SEARCH PHOTO"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PhotoProject/TabBarController.swift
@@ -0,0 +1,44 @@
+//
+//  TabBarController.swift
+//  PhotoProject
+//
+//  Created by 이빈 on 1/17/25.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabBarController()
+        configureTabBarAppearance()
+    }
+    
+    func configureTabBarController() {
+        let firstVC = TopicViewController()
+        firstVC.tabBarItem.image = UIImage(systemName: "chart.line.uptrend.xyaxis")
+        
+        let secondVC = SecondViewController()
+        secondVC.tabBarItem.image = UIImage(systemName: "film")
+        
+        let thirdVC = PhotoSearchViewController()
+        thirdVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        let thirdNav = UINavigationController(rootViewController: thirdVC)
+        
+        let fourthVC = FourthViewController()
+        fourthVC.tabBarItem.image = UIImage(systemName: "heart")
+        
+        setViewControllers([firstVC, secondVC, thirdNav, fourthVC], animated: true)
+    }
+    
+    func configureTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .white
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.tintColor = .black
+    }
+
+}
