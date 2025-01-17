bbf8525618ae22f1d0e7ac14d6e9becd19cc7e44
been <leebin0603@naver.com>
2025-01-17 22:12:44 +0900

    Feat: BaseCollectionViewCell 세팅

---
+++ PhotoProject/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BaseCollectionViewCell.swift
+//  PhotoProject
+//
+//  Created by 이빈 on 1/17/25.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+    func configureView() { }
+    
+}
