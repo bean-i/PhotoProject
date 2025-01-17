47a76fa64390ae41297744cf6b7064902c935109
been <leebin0603@naver.com>
2025-01-17 22:10:26 +0900

    Chore: gitignore 및 APIKey 설정

---
+++ PhotoProject/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  PhotoProject
+//
+//  Created by 이빈 on 1/17/25.
+//
+
+import Foundation
+
+enum SearchPhotoAPI {
+    static let clientID = "AbuJtnH-3d5g0y0QT-VzFKrX5MDFgDe_CrPpvC4_ck8"
+}
